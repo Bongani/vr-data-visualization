f5f0062fe7a8c26098e921979564e564fe003123
Nanne Aben <nanneaben@Nannes-MacBook-Pro-2.local>
2019-03-26 13:29:52 +0100

    Plotting using triangles, minimal movement

--- app/src/main/java/ing/wbaa/vrdatavisualization/MainActivity.kt
+++ app/src/main/java/ing/wbaa/vrdatavisualization/MainActivity.kt
@@ -5,67 +5,54 @@ import android.opengl.Matrix
 import android.os.Bundle
 import android.util.Log
 import com.google.vr.sdk.base.*
-import java.io.IOException
 import java.util.*
 import javax.microedition.khronos.egl.EGLConfig
 import org.jetbrains.anko.*
 import java.net.URL
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.FloatBuffer
 
 class MainActivity : GvrActivity(), GvrView.StereoRenderer {
     private val TAG = "HelloVrActivity"
 
+    private val COORDS_PER_VERTEX = 3
+    private val triCoords = floatArrayOf(
+        0.0f,  0.6f, -1.0f, // top
+        -0.5f, -0.3f, -1.0f, // bottom left
+        0.5f, -0.3f, -1.0f  // bottom right
+    )
+    private val triVertexCount = triCoords.size / COORDS_PER_VERTEX
+    private var triVerticesBuffer: FloatBuffer? = null
+
+    private var simpleVertexShader: Int = 0
+    private var simpleFragmentShader: Int = 0
+    private var triProgram: Int = 0
+    private var triColorParam: Int = 0
+    private var triPositionParam: Int = 0
+    private var triMVPMatrixParam: Int = 0
+
     private val Z_NEAR = 0.01f
     private val Z_FAR = 10.0f
 
-    private val MIN_TARGET_DISTANCE = 3.0f
-
-    private val OBJECT_VERTEX_SHADER_CODE = arrayOf(
-        "uniform mat4 u_MVP;",
-        "attribute vec4 a_Position;",
-        "attribute vec2 a_UV;",
-        "varying vec2 v_UV;",
-        "",
-        "void main() {",
-        "  v_UV = a_UV;",
-        "  gl_Position = u_MVP * a_Position;",
-        "}"
-    )
-    private val OBJECT_FRAGMENT_SHADER_CODE = arrayOf(
-        "precision mediump float;",
-        "varying vec2 v_UV;",
-        "uniform sampler2D u_Texture;",
-        "",
-        "void main() {",
-        "  // The y coordinate of this sample's textures is reversed compared to",
-        "  // what OpenGL expects, so we invert the y coordinate.",
-        "  gl_FragColor = texture2D(u_Texture, vec2(v_UV.x, 1.0 - v_UV.y));",
-        "}"
-    )
-
-    private var objectProgram: Int = 0
-
-    private var objectPositionParam: Int = 0
-    private var objectUvParam: Int = 0
-    private var objectModelViewProjectionParam: Int = 0
-
-    private var targetObjectMeshes: TexturedMesh? = null
-    private var targetObjectNotSelectedTextures: ArrayList<Texture>? = null
-
-    private var targetPosition: FloatArray = floatArrayOf()
     private var camera: FloatArray = floatArrayOf()
     private var view: FloatArray = floatArrayOf()
     private var headView: FloatArray = floatArrayOf()
-    private var modelViewProjection: FloatArray = floatArrayOf()
-    private var modelView: FloatArray = floatArrayOf(0f,0f,0f,0f)
-
     private var modelTarget: FloatArray = floatArrayOf(0f,0f,0f,0f)
+    private var modelView: FloatArray = floatArrayOf(0f,0f,0f,0f)
+    private var modelViewProjection: FloatArray = floatArrayOf()
 
-    private var tempPosition: FloatArray = floatArrayOf(0f,0f,0f,0f)
     private var headRotation: FloatArray = floatArrayOf(0f,0f,0f,0f)
+
+    private var cols = ArrayList<FloatArray>()
 
     private var points = ArrayList<FloatArray>()
     private var labels = ArrayList<Int>()
-    private var curTargetObject: Int = 0
+
+    private var curLabel: Int = 0
+    private var currentPosition: FloatArray = floatArrayOf()
+
+    private var cameraPos: Float = 0.0f
 
     /**
      * Sets the view to our GvrView and initializes the transformation matrices we will use
@@ -73,40 +60,27 @@ class MainActivity : GvrActivity(), GvrView.StereoRenderer {
      */
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         initializeGvrView()
+        readData()
 
         camera = FloatArray(16)
         view = FloatArray(16)
-        modelViewProjection = FloatArray(16)
-        modelView = FloatArray(16)
-        // Target object first appears directly in front of user.
-        targetPosition = floatArrayOf(0.0f, 0.0f, -MIN_TARGET_DISTANCE)
-        tempPosition = FloatArray(4)
         headRotation = FloatArray(4)
-        modelTarget = FloatArray(16)
         headView = FloatArray(16)
+        modelTarget = FloatArray(16)
+        modelView = FloatArray(16)
+        modelViewProjection = FloatArray(16)
 
-        doAsync {
-            val file = URL("https://topwebers.com/mnist.csv").readText()
-            uiThread {
-                val lines = file.split("\n").toTypedArray()
-                var i = 0
-                for(line in lines) {
-                    Log.d("hoi2",i.toString())
-                    Log.d("hoi2",(i%4).toString())
-                    if(line!="" && i < 2000 && i%4==0) {
-                        val nums = line.split(",").toTypedArray()
-                        points.add(floatArrayOf(nums[0].toFloat()/2,
-                            nums[1].toFloat()/2,
-                            nums[2].toFloat()/2))
-                        labels.add(nums[3].toFloat().toInt())
-                        Log.d("hoi1", nums[3])
-                    }
-                i += 1
-                }
-            }
-        }
+        cols.add(floatArrayOf(1.0f, 0.0f, 0.0f, 0.0f))
+        cols.add(floatArrayOf(0.0f, 1.0f, 0.0f, 0.0f))
+        cols.add(floatArrayOf(0.0f, 0.0f, 1.0f, 0.0f))
+        cols.add(floatArrayOf(0.5f, 0.5f, 0.5f, 0.0f))
+        cols.add(floatArrayOf(0.5f, 0.5f, 0.0f, 0.0f))
+        cols.add(floatArrayOf(0.5f, 0.0f, 0.5f, 0.0f))
+        cols.add(floatArrayOf(0.0f, 0.5f, 0.5f, 0.0f))
+        cols.add(floatArrayOf(0.0f, 0.0f, 0.5f, 0.0f))
+        cols.add(floatArrayOf(0.0f, 0.5f, 0.0f, 0.0f))
+        cols.add(floatArrayOf(0.5f, 0.0f, 0.0f, 0.0f))
     }
 
     fun initializeGvrView() {
@@ -132,6 +106,27 @@ class MainActivity : GvrActivity(), GvrView.StereoRenderer {
         setGvrView(gvrView)
     }
 
+    private fun readData() {
+        doAsync {
+            val file = URL("https://topwebers.com/mnist.csv").readText()
+            uiThread {
+                val lines = file.split("\n").toTypedArray()
+                var i = 0
+                for(line in lines) {
+                    if(line!="" && i%5==0) {
+                        val nums = line.split(",").toTypedArray()
+                        points.add(floatArrayOf(nums[0].toFloat()/2,
+                            nums[1].toFloat()/2,
+                            nums[2].toFloat()/2))
+                        labels.add(nums[3].toFloat().toInt())
+                    }
+                    i += 1
+                    Log.i(TAG, i.toString())
+                }
+            }
+        }
+    }
+
     override fun onRendererShutdown() {
         Log.i(TAG, "onRendererShutdown")
     }
@@ -152,33 +147,77 @@ class MainActivity : GvrActivity(), GvrView.StereoRenderer {
     override fun onSurfaceCreated(config: EGLConfig) {
         Log.i(TAG, "onSurfaceCreated")
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 0.0f)
+        compileShaders()
+        prepareRenderingTriangle()
+    }
 
-        objectProgram = Util.compileProgram(OBJECT_VERTEX_SHADER_CODE, OBJECT_FRAGMENT_SHADER_CODE)
+    private fun compileShaders() {
+        var shader = "uniform mat4 u_MVP;\n" +
+                "attribute vec4 a_Position;\n" +
+                "void main() {\n" +
+                "   gl_Position = u_MVP * a_Position;\n" +
+                "}\n"
+        simpleVertexShader = loadShader(GLES20.GL_VERTEX_SHADER, shader)
 
-        objectPositionParam = GLES20.glGetAttribLocation(objectProgram, "a_Position")
-        objectUvParam = GLES20.glGetAttribLocation(objectProgram, "a_UV")
-        objectModelViewProjectionParam = GLES20.glGetUniformLocation(objectProgram, "u_MVP")
+        shader = "precision mediump float;\n" +
+                "uniform vec4 u_Color;\n" +
+                "void main() {\n" +
+                "    gl_FragColor = u_Color;\n" +
+                "}"
+        simpleFragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, shader)
+    }
 
-        Util.checkGlError("Object program params")
+    private fun loadShader(type: Int, code: String): Int {
+        val shader = GLES20.glCreateShader(type)
 
-        updateTargetPosition()
+        // add the source code to the shader and compile it
+        GLES20.glShaderSource(shader, code)
+        GLES20.glCompileShader(shader)
 
-        Util.checkGlError("onSurfaceCreated")
-        Log.d("hoi", "onsurfacecreated")
-        try {
-            targetObjectNotSelectedTextures = ArrayList<Texture>()
-            targetObjectNotSelectedTextures!!.add(Texture(this, "QuadSphere_Blue_BakedDiffuse.png"))
-            targetObjectNotSelectedTextures!!.add(Texture(this, "QuadSphere_Pink_BakedDiffuse.png"))
-            targetObjectMeshes = TexturedMesh(this, "QuadSphere.obj", objectPositionParam, objectUvParam)
-        } catch (e: IOException) {
-            Log.e(TAG, "Unable to initialize objects", e)
-        }
+        return shader
+    }
+
+    private fun prepareRenderingTriangle() {
+        // Allocate buffers
+        // initialize vertex byte buffer for shape coordinates (4 */ bytes per float)
+        val bb = ByteBuffer.allocateDirect(triCoords.size * 4)
+        // use the device hardware's native byte order
+        bb.order(ByteOrder.nativeOrder())
+
+        // create a floating point buffer from the ByteBuffer
+        triVerticesBuffer = bb.asFloatBuffer()
+        // add the coordinates to the FloatBuffer
+        triVerticesBuffer?.put(triCoords)
+        // set the buffer to read the first coordinate
+        triVerticesBuffer?.position(0)
+
+        // Create GL program
+        // create empty OpenGL ES Program
+        triProgram = GLES20.glCreateProgram()
+        // add the vertex shader to program
+        GLES20.glAttachShader(triProgram, simpleVertexShader)
+        // add the fragment shader to program
+        GLES20.glAttachShader(triProgram, simpleFragmentShader)
+        // build OpenGL ES program executable
+        GLES20.glLinkProgram(triProgram)
+        // set program as current
+        GLES20.glUseProgram(triProgram)
+
+        // Get shader params
+        // get handle to vertex shader's a_Position member
+        triPositionParam = GLES20.glGetAttribLocation(triProgram, "a_Position")
+        // enable a handle to the triangle vertices
+        GLES20.glEnableVertexAttribArray(triPositionParam)
+        // get handle to fragment shader's u_Color member
+        triColorParam = GLES20.glGetUniformLocation(triProgram, "u_Color")
+        // get handle to shape's transformation matrix
+        triMVPMatrixParam = GLES20.glGetUniformLocation(triProgram, "u_MVP")
     }
 
     /** Updates the target object position.  */
     private fun updateTargetPosition() {
         Matrix.setIdentityM(modelTarget, 0)
-        Matrix.translateM(modelTarget, 0, targetPosition!![0], targetPosition!![1], targetPosition!![2])
+        Matrix.translateM(modelTarget, 0, currentPosition!![0], currentPosition!![1], currentPosition!![2])
 
         Util.checkGlError("updateTargetPosition")
     }
@@ -190,7 +229,7 @@ class MainActivity : GvrActivity(), GvrView.StereoRenderer {
      */
     override fun onNewFrame(headTransform: HeadTransform) {
         // Build the camera matrix and apply it to the ModelView.
-        Matrix.setLookAtM(camera, 0, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f)
+        Matrix.setLookAtM(camera, 0, cameraPos, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f)
 
         headTransform.getHeadView(headView!!, 0)
 
@@ -206,6 +245,11 @@ class MainActivity : GvrActivity(), GvrView.StereoRenderer {
      * @param eye The eye to render. Includes all required transformations.
      */
     override fun onDrawEye(eye: Eye) {
+//        val a = floatArrayOf(1.0f,0.0f,0.0f,0.0f)
+//        Matrix.rotateM(a, 0,
+//            headRotation[0], headRotation[1],
+//            headRotation[2], headRotation[3])
+
         GLES20.glEnable(GLES20.GL_DEPTH_TEST)
         // The clear color doesn't matter here because it's completely obscured by
         // the room. However, the color buffer is still cleared because it may
@@ -222,31 +266,43 @@ class MainActivity : GvrActivity(), GvrView.StereoRenderer {
         val it = points.iterator()
         val it_label = labels.iterator()
         while (it.hasNext()) {
-            targetPosition = it.next()
-            curTargetObject = it_label.next()
+            currentPosition = it.next()
+            curLabel = it_label.next()
             updateTargetPosition()
             Matrix.multiplyMM(modelView, 0, view, 0, modelTarget, 0)
             Matrix.multiplyMM(modelViewProjection, 0, perspective, 0, modelView, 0)
-            drawTarget()
+            drawTriangle()
         }
     }
 
-    override fun onFinishFrame(viewport: Viewport) {}
+    private fun drawTriangle() {
+        // Add program to OpenGL ES environment
+        GLES20.glUseProgram(triProgram)
 
-    /** Draw the target object.  */
-    fun drawTarget() {
-        GLES20.glUseProgram(objectProgram)
-        GLES20.glUniformMatrix4fv(objectModelViewProjectionParam, 1, false, modelViewProjection, 0)
-        targetObjectNotSelectedTextures!![curTargetObject].bind()
-        Log.d("hoi", curTargetObject.toString())
-        targetObjectMeshes!!.draw()
-        Util.checkGlError("drawTarget")
+        // Pass the MVP transformation to the shader
+        GLES20.glUniformMatrix4fv(triMVPMatrixParam, 1, false, modelViewProjection, 0)
+
+        // Prepare the coordinate data
+        GLES20.glVertexAttribPointer(
+            triPositionParam, COORDS_PER_VERTEX,
+            GLES20.GL_FLOAT, false, 0, triVerticesBuffer
+        )
+
+        // Set color for drawing
+        val col = cols.get(curLabel)
+        GLES20.glUniform4fv(triColorParam, 1, col, 0)
+
+        // Draw the model
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, triVertexCount)
     }
+
+    override fun onFinishFrame(viewport: Viewport) {}
 
     /**
      * Called when the Cardboard trigger is pulled.
      */
     override fun onCardboardTrigger() {
-        Log.i(TAG, "onCardboardTrigger")
+        cameraPos += 1
+        Log.i("hoi", "onCardboardTrigger")
     }
 }
